23bef442011f66d0148de7a474391f8eff6fb887
Fred Eisele <feisele@isis.vanderbilt.edu>
2015-12-18 15:55:22 -0600

    creating the first webgme plugins for interacting with tinkerpop3, this was generated using https://github.com/webgme/webgme-setup-tool

---
+++ config/config.default.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var config = require('./config.webgme'),
+    validateConfig = require('webgme/config/validator');
+
+// Add/overwrite any additional settings here
+// config.server.port = 8080;
+// config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
+
+config.plugin.allowBrowserExecution = true
+config.plugin.allowServerExecution = false
+config.seedProjects.enable = true
+
+
+validateConfig(config);
+module.exports = config;
