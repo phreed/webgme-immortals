45d4acba30f3ea078981e4476b22bf2fcc994d3b
Brass Immortals <fred.eisele@vanderbilt.edu>
2016-04-13 15:04:08 -0500

    made some small changes related to correct mongodb versions

--- config/config.default.js
+++ config/config.default.js
@@ -7,9 +7,13 @@ var config = require('./config.webgme'),
 // config.server.port = 8080;
 // config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
 
+// See config.immortals.js for server settings.
+
 config.plugin.allowBrowserExecution = true;
 config.plugin.allowServerExecution = false;
+
 config.seedProjects.enable = true;
+config.seedProjects.allowDuplication = true;
 
 config.requirejsPaths["cytoscape"] = './node_modules/cytoscape/dist/';
 config.requirejsPaths["bower"] = './bower_modules/';
@@ -18,6 +22,7 @@ config.requirejsPaths["style"] = './style/';
 // so that there can be interaction with Jena
 
 config.visualization.extraCss = ['style/immortals.css'];
+
 
 validateConfig(config);
 module.exports = config;
