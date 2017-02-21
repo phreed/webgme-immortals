d84430fc9feb07ed40d04ae86fe702be70a4310a
Fred Eisele <fred.eisele@vanderbilt.isis>
2017-02-21 08:51:11 -0600

    Update versions

--- init/stream_plugin.sh
+++ init/stream_plugin.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+function stream_network {
 node ./dist-client/main/run_plugin.js \
   StreamPlugin \
   immortals_v03_cp02_network \
   --owner fred \
   --user fred \
   --branchName master
+}
+
+function stream_feature {
+node ./dist-client/main/run_plugin.js \
+  StreamPlugin \
+  immortals_v03_cp02_network \
+  --owner fred \
+  --user fred \
+  --branchName master
+}
+
+select stream in "feature" "network" "exit"
+do
+  case "$stream" in
+  ("exit") break;;
+  ("feature") stream_feature;;
+  ("network") stream_network;;
+  (*) echo "bad stream request";;
+  esac
+done
+exit 0
+
+
